14a30c8c042990e4924a7fcd5759eeeaad19b24d
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-09-04 16:54:18 +0300

    Add model and serializer

---
+++ cinema/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from cinema.models import Movie
+
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=True, max_length=255)
+    description = serializers.CharField(required=True)
+    duration = serializers.IntegerField(required=True)
+
+    def create(self, validated_data) -> Movie:
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data) -> Movie:
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get(
+            "description",
+            instance.description
+        )
+        instance.duration = validated_data.get("duration", instance.duration)
+        instance.save()
+        return instance
--- movie_api/settings.py
+++ movie_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "cinema",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
